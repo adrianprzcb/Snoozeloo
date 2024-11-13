3cabe268832ee534460610b70a4f00169cf0c4ce
adrianprzcb <adrianpc4444@gmail.com>
2024-11-13 18:25:10 +0100

    None.

--- app/src/main/java/com/adrian/snoozeloo/domain/repositories/AlarmRepository.kt
+++ app/src/main/java/com/adrian/snoozeloo/domain/repositories/AlarmRepository.kt
@@ -3,6 +3,7 @@ package com.adrian.snoozeloo.domain.repositories
 import com.adrian.snoozeloo.domain.model.Alarm
 import kotlinx.coroutines.flow.Flow
 
+
 interface AlarmRepository {
     fun getAllAlarms(): Flow<List<Alarm>>
     suspend fun insertAlarm(alarm: Alarm)

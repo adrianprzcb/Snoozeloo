3026ddaab2b9dd3ac6cbb20dc83e6a88537f4726
adrianprzcb <adrianpc4444@gmail.com>
2024-11-25 23:40:58 +0100

    ud

--- app/src/main/java/com/adrian/snoozeloo/data/database/Converters.kt
+++ app/src/main/java/com/adrian/snoozeloo/data/database/Converters.kt
@@ -1,4 +1,15 @@
 package com.adrian.snoozeloo.data.database
 
+import androidx.room.TypeConverter
+import com.google.gson.Gson
+import com.google.gson.reflect.TypeToken
+
 class Converters {
+
+    @TypeConverter
+    fun fromStringList(value: String): List<String> {
+        val listType = object : TypeToken<List<String>>() {}.type
+        return Gson().fromJson(value, listType)
+    }
+
 }

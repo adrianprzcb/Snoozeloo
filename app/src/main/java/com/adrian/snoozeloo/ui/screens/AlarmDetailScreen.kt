da6052b5de59f2bd83800ec03f93326d54de03c0
adrianprzcb <adrianpc4444@gmail.com>
2024-11-27 00:11:20 +0100

    ud

--- app/src/main/java/com/adrian/snoozeloo/ui/screens/AlarmDetailScreen.kt
+++ app/src/main/java/com/adrian/snoozeloo/ui/screens/AlarmDetailScreen.kt
@@ -1,10 +1,19 @@
 package com.adrian.snoozeloo.ui.screens
 
 import androidx.compose.runtime.Composable
+import com.adrian.snoozeloo.data.model.AlarmData
 
 @Composable
 fun AlarmDetailScreen(
     onSaveAlarm: (AlarmData) -> Unit,
     navigateToRingtoneSettings: () -> Unit){
 
+}
+
+
+
+fun validateTime(hours: String, minutes: String): Boolean {
+    val h = hours.toIntOrNull()
+    val m = minutes.toIntOrNull()
+    return h in 0..23 && m in 0..59
 }

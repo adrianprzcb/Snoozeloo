52ddf0dd0f143de643e34df66c1da009d4f75279
adrianprzcb <adrianpc4444@gmail.com>
2024-11-26 12:07:26 +0100

    ud

--- app/src/main/java/com/adrian/snoozeloo/ui/components/AlarmList.kt
+++ app/src/main/java/com/adrian/snoozeloo/ui/components/AlarmList.kt
@@ -1,10 +1,17 @@
 package com.adrian.snoozeloo.ui.components
 
 import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.material3.Card
+import androidx.compose.material3.CardDefaults
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
 import com.adrian.snoozeloo.data.model.Alarm
 
 @Composable
@@ -15,5 +22,32 @@ fun AlarmList (
     LazyColumn (
         modifier = Modifier.fillMaxSize(),
         contentPadding = PaddingValues()
-    ){  }
+    ){
+        items(alarms.size){ index ->
+            AlarmItem()
+        }
+    }
+}
+
+@Composable
+fun AlarmItem(
+    alarm: Alarm,
+    onToggle: (Alarm) -> Unit
+) {
+    Card (
+        modifier = Modifier
+            .fillMaxWidth()
+            .padding(horizontal = 16.dp , vertical = 8.dp),
+        elevation = CardDefaults.cardElevation(4.dp)
+    ){
+        Row(
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(16.dp),
+            verticalAlignment = Alignment.CenterVertically
+        ){
+
+        }
+
+    }
 }

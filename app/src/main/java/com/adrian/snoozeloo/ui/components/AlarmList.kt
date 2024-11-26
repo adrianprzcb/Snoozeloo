c71b8b8b7a8ea0921bcdb548fc00ba33d2120cb4
adrianprzcb <adrianpc4444@gmail.com>
2024-11-26 12:20:30 +0100

    ud

--- app/src/main/java/com/adrian/snoozeloo/ui/components/AlarmList.kt
+++ app/src/main/java/com/adrian/snoozeloo/ui/components/AlarmList.kt
@@ -1,5 +1,6 @@
 package com.adrian.snoozeloo.ui.components
 
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
@@ -8,6 +9,8 @@ import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.material3.Card
 import androidx.compose.material3.CardDefaults
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
@@ -49,7 +52,18 @@ fun AlarmItem(
                 .padding(16.dp),
             verticalAlignment = Alignment.CenterVertically
         ){
-
+            Column (
+                modifier = Modifier.weight(1f)
+            ){
+                Text(
+                    text = alarm.alarmName ?: "Untitled Alarm",
+                    style = MaterialTheme.typography.titleMedium
+                )
+                Text(
+                    text = "Next: ${alarm.nextOccurrence}", //Format this dynamically
+                    style = MaterialTheme.typography.bodyMedium
+                )
+            }
         }
 
     }

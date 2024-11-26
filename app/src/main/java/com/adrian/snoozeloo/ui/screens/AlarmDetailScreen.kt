1dd988f14f42f60a088d1e857535fffed2d4c142
adrianprzcb <adrianpc4444@gmail.com>
2024-11-27 00:29:16 +0100

    ud

--- app/src/main/java/com/adrian/snoozeloo/ui/screens/AlarmDetailScreen.kt
+++ app/src/main/java/com/adrian/snoozeloo/ui/screens/AlarmDetailScreen.kt
@@ -1,11 +1,23 @@
 package com.adrian.snoozeloo.ui.screens
 
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.text.KeyboardOptions
+import androidx.compose.material3.OutlinedTextField
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
+import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.text.input.KeyboardType
+import androidx.compose.ui.unit.dp
+import androidx.core.text.isDigitsOnly
 import com.adrian.snoozeloo.data.model.AlarmData
 
 @Composable
@@ -25,6 +37,34 @@ fun AlarmDetailScreen(
 
     val isTimeValid = validateTime(hours, minutes)
     val context = LocalContext.current
+
+
+
+    Column (
+        modifier = Modifier.fillMaxSize()
+            .padding(16.dp),
+        verticalArrangement = Arrangement.spacedBy(16.dp)
+    ){
+        //Input fields for hours and minutes
+        Row(
+            horizontalArrangement = Arrangement.spacedBy(16.dp)
+        ) {
+            OutlinedTextField(
+                value = hours,
+                onValueChange = {if (it.isDigitsOnly()) hours = it},
+                label = { Text("Hours") },
+                keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Number),
+                modifier = Modifier.weight(1f)
+            )
+            OutlinedTextField(
+                value = minutes,
+                onValueChange = {if (it.isDigitsOnly()) minutes = it},
+                label = { Text("Minutes") },
+                keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Number)
+            )
+        }
+
+    }
 
 
 

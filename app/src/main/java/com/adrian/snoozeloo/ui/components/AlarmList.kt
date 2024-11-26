0913003ac35cf00001135089b7c59228f29901fc
adrianprzcb <adrianpc4444@gmail.com>
2024-11-26 12:21:00 +0100

    ud

--- app/src/main/java/com/adrian/snoozeloo/ui/components/AlarmList.kt
+++ app/src/main/java/com/adrian/snoozeloo/ui/components/AlarmList.kt
@@ -10,6 +10,7 @@ import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.material3.Card
 import androidx.compose.material3.CardDefaults
 import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Switch
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
@@ -64,6 +65,10 @@ fun AlarmItem(
                     style = MaterialTheme.typography.bodyMedium
                 )
             }
+            Switch(
+                checked = alarm.isEnabled,
+                onCheckedChange = {onToggle(alarm)}
+            )
         }
 
     }

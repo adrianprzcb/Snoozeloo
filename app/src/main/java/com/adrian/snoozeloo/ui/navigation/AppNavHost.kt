c425e274e1bc4055bdc5f94dbcb359a685b1b1fe
adrianprzcb <adrianpc4444@gmail.com>
2024-11-27 00:38:23 +0100

    ud

--- app/src/main/java/com/adrian/snoozeloo/ui/navigation/AppNavHost.kt
+++ app/src/main/java/com/adrian/snoozeloo/ui/navigation/AppNavHost.kt
@@ -3,7 +3,10 @@ package com.adrian.snoozeloo.ui.navigation
 import androidx.compose.runtime.Composable
 import androidx.navigation.compose.NavHost
 import androidx.navigation.NavHostController
+import androidx.navigation.NavType
 import androidx.navigation.compose.composable
+import androidx.navigation.navArgument
+import com.adrian.snoozeloo.ui.screens.AlarmDetailScreen
 import com.adrian.snoozeloo.ui.screens.AlarmListScreen
 import com.adrian.snoozeloo.viewmodel.AlarmViewModel
 
@@ -22,9 +25,28 @@ fun AppNavHost(
              onAddAlarm = {navController.navigate("alarm_detail")}
          )
         }
-        composable("alarm_detail") {
-
+        // Alarm detail screen for adding or editing
+        composable(
+            route = "alarm_detail",
+            arguments = listOf(
+                navArgument("alarmId") {
+                    type = NavType.StringType
+                    nullable = true // Allow null for creating a new alarm
+                    defaultValue = null
+                }
+            )
+        ) { backStackEntry ->
+            val alarmId = backStackEntry.arguments?.getString("alarmId")
+            AlarmDetailScreen(
+                alarm = alarmId?.let { alarmViewModel.getAlarmById(it) },
+                onSaveAlarm = { alarm ->
+                    alarmViewModel.saveAlarm(alarm)
+                    navController.popBackStack() // Navigate back to the list
+                },
+                navigateToRingtoneSettings = {
+                    navController.navigate("ringtone_settings")
+                }
+            )
         }
-
     }
 }

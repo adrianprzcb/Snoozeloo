93f8e9ecc40ad6cc7be5a2473b63ce307878717a
adrianprzcb <adrianpc4444@gmail.com>
2024-11-26 12:02:10 +0100

    ud

--- app/src/main/java/com/adrian/snoozeloo/ui/components/AlarmList.kt
+++ app/src/main/java/com/adrian/snoozeloo/ui/components/AlarmList.kt
@@ -1,9 +1,19 @@
 package com.adrian.snoozeloo.ui.components
 
+import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import com.adrian.snoozeloo.data.model.Alarm
 
 @Composable
 fun AlarmList (
+    alarms: List<Alarm>,
+    onToggle: (Alarm) -> Unit
 ){
-
+    LazyColumn (
+        modifier = Modifier.fillMaxSize(),
+        contentPadding = PaddingValues()
+    ){  }
 }

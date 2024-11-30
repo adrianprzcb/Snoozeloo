ceb358a0a4a924d5946bb8bcf269d94d97b123c3
adrianprzcb <adrianpc4444@gmail.com>
2024-11-30 23:24:52 +0100

    ud

--- app/src/main/java/com/adrian/snoozeloo/ui/components/AlarmList.kt
+++ app/src/main/java/com/adrian/snoozeloo/ui/components/AlarmList.kt
@@ -5,6 +5,7 @@ import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.material3.Card
@@ -53,7 +54,7 @@ fun AlarmItem(
     Card (
         modifier = Modifier
             .fillMaxWidth()
-            .padding(horizontal = 16.dp , vertical = 8.dp),
+            .padding(horizontal = 16.dp , vertical = 8.dp) ,
         elevation = CardDefaults.cardElevation(4.dp)
     ){
         Row(
